613756dbb50b483d736b0f124509193dff256ed7
Tomoyuki Hata <hato6502@gmail.com>
2020-11-13 00:27:16 +0900

    add dynamo db (#7)
    * add dynamo db
    
    * add duplication check
    
    * attach policies

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import * as AWS from "aws-sdk";
+import * as crypto from "crypto";
 import fetch from "node-fetch";
 import * as Twitter from "twitter";
 
 const handler = async (): Promise<void> => {
+  const dynamoDB = new AWS.DynamoDB({
+    apiVersion: "2012-08-10",
+    endpoint:
+      process.env.DYNAMODB_ENDPOINT === ""
+        ? undefined
+        : process.env.DYNAMODB_ENDPOINT,
+  });
+
   const rekognition = new AWS.Rekognition();
+
   const twitter = new Twitter({
     /* eslint-disable @typescript-eslint/camelcase */
-    consumer_key: process.env.TWITTER_CONSUMER_KEY || "",
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET || "",
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY || "",
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET || "",
+    consumer_key: process.env.TWITTER_CONSUMER_KEY ?? "",
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET ?? "",
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY ?? "",
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET ?? "",
     /* eslint-enable @typescript-eslint/camelcase */
   });
 
@@ -35,6 +45,21 @@ const handler = async (): Promise<void> => {
 
     const image = await fetch(firstMedia.media_url_https);
     const imageBuffer = await image.buffer();
+    const id = crypto.createHash("sha256").update(imageBuffer).digest("hex");
+
+    const item = await dynamoDB
+      .getItem({
+        TableName: process.env.DYNAMODB_TABLE_NAME ?? "",
+        Key: {
+          id: { S: id },
+        },
+      })
+      .promise();
+
+    if (item.Item) {
+      continue;
+    }
+
     const { Labels } = await rekognition
       .detectLabels({
         Image: {
@@ -65,6 +90,15 @@ const handler = async (): Promise<void> => {
     if (!ModerationLabels || ModerationLabels.length !== 0) {
       continue;
     }
+
+    await dynamoDB
+      .putItem({
+        TableName: process.env.DYNAMODB_TABLE_NAME ?? "",
+        Item: {
+          id: { S: id },
+        },
+      })
+      .promise();
 
     await twitter.post("favorites/create", { id: status.id_str });
     await twitter.post("statuses/retweet", { id: status.id_str });
